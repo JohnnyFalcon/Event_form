dcb200eee8f50624774848fa94514923454a42ea
Johnny <sokolowski8.kuba@gmail.com>
2023-10-26 08:12:41 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 # exit on error
 set -o errexit
 
+poetry install
+
 pip install --upgrade pip 
+pip install --force-reinstall -U setuptools
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
