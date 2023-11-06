e07c62b91baad781b7e6d80807aab9be514c6521
Jakub Sokołowski <Sokolowski8.kuba@gmail.com>
2023-11-06 08:29:06 +0100

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views import home, about
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("eventapp.urls")),
+    path("", home, name="home"),
+    path("about", about, name="about"),
 ]
